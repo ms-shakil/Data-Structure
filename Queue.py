254922865d143fa4e273b1b9aea26b90f7955bea
ms-shakil <mdshakil8796@gmail.com>
2021-05-30 08:56:52 +0600

    about queue

---
+++ Queue.py
@@ -0,0 +1,23 @@
+### about queue data structure 
+class Queue:
+    def __init__(self):
+        self.arr =[]
+        
+    def Enqueue(self,data):
+        self.arr.append(data)    
+    
+    def Dequeue(self):
+        return self.arr.pop(0)
+    def is_empty(self):
+        if self.arr ==[]:
+            return True
+        return False      
+        
+Q = Queue()
+Q.Enqueue("Shakil")
+Q.Enqueue("Sajjad")
+
+
+while not Q.is_empty():
+    p = Q.Dequeue()
+    print(p)
